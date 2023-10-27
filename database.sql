a510563fa2188ed796f0017aa58d8c3cf545700f
pedrormedeiros <perodriigues39@gmail.com>
2023-10-27 12:04:21 -0300

    Script restruturado

--- database.sql
+++ database.sql
@@ -1,61 +1,29 @@
-/* Mostrar os bancos de dados existentes no servidor */
-show databases;
+/* Banco de dados para gerenciamento de hotel */
 
-/* Criar um novo banco */ 
 create database dbHotel;
 
-/* Selecionar o banco de dados */
+show databases;
+
 use dbHotel;
 
-/* Excluir tabelas */
-drop table funcionarios;
-
-/* Adicionar o campo email à tabela funcionarios */
-alter table funcionarios add column email varchar (50) not null;
-
-alter table funcionarios add column nomeFunc varchar(100) not null after idFunc;
-
-/* Excluir um campo da tabela */
-alter table funcionarios drop column email;
-
-/* Redirecionar o campo email para que ele fique após o campo login */
-alter table funcionarios modify column email varchar(50) not null after login;
-
-/* Modificar o campo email para que se torne obrigatório, ou seja, não nulo (not null) */
-alter table  funcionarios modify column email varchar(50) not null;
-
-/*Criar uma nova tabela */
 create table funcionarios (
-	/* int se refere ao tipo na tabela 
-    primary key determina que o campo idFunc seja uma chave primária e será responsável popr identificar cada funcionário
-    auto_increment determina que o MySQL dê um id novo a cada cadastro */
     idFunc int primary key auto_increment,
-    /* varchar é a quantidade variável de caracteres no nome (nesse caso, até 100 caracteres)
-    not null determina que o campo seja obrigatório */
-    
-    /* unique determina que o campo seja único e o valor não possa repetir */
+    nomeFunc varchar (100) not null,
     login varchar (20) not null unique,
+    email varchar (50) not null,
     senha varchar (255) not null,
     cargo varchar(20)
 );
 
-/* Descreve os campos da tabela funcionarios */
 describe funcionarios;
 
-
-
-/* Mostrar as tabelas */
-show tables;
-
-/*CREATE */
-/* inserir um funcionario na tabela, com nome, login, senha criptografada  e cargo */
 insert into funcionarios(nomeFunc, login, senha, cargo, email) values ("Administrador", "admin", md5("admin"), "Administrador", "admin@gmail.com");
 
-insert into funcionarios(nomeFunc, login, senha, cargo) values ("Pamella Pereto", "pamellapereto", md5("123@senac"), "TI");
+insert into funcionarios(nomeFunc, login, senha, cargo, email) values ("Pamella Pereto", "pamellapereto", md5("123@senac"), "TI", "pamellapereto@gmail.com");
 
-insert into funcionarios(nomeFunc, login, senha, cargo) values ("Breno Silva", "brenosilva", md5("123@senac"), "Contabilidade");
+insert into funcionarios(nomeFunc, login, senha, cargo, email) values ("Breno Silva", "brenosilva", md5("123@senac"), "Contabilidade", "brenosilva@gmail.com");
 
-insert into funcionarios(nomeFunc, login, senha, cargo) values ("Victoria Cardoso", "victoriacardoso", md5("123@senac"), "RH");
+insert into funcionarios(nomeFunc, login, senha, cargo, email) values ("Victoria Cardoso", "victoriacardoso", md5("123@senac"), "Gerência", "victoriacardoso@gmail.com");
 
 insert into funcionarios(nomeFunc, login, senha, cargo, email) values ("Nathan Raphael", "nathanraphael", md5("123@senac"), "Gerência", "nathanraphael@gmail.com");
 
@@ -63,92 +31,49 @@ insert into funcionarios(nomeFunc, login, senha) values ("Laura Lopes", "lauralo
 
 insert into funcionarios(nomeFunc, login, senha) values ("Felipe Coelho", "felipe", md5("123@senac"));
 
-/*READ*/
-/* Ler/Buscar as informações da tabela funcionários */ 
 select * from funcionarios;
-
-/* Buscar o login e a senha da tabela funcionarios em que o login seja admin e senha seja admin */
 select login as Login, senha from funcionarios where login = "admin" and senha =md5("admin");
 
-/*Buscar o ID e o nome do funcionário da tabela funcionários ordenado o nome alfabeticamente (ascendente, de A a Z) */
 select idFunc as ID_Funcionario, nomeFunc as Nome_Funcionario from funcionarios order by nomeFunc asc;
 
-/*Buscar o ID e o nome do funcionário da tabela funcionários ordenado o nome alfabeticamente (descendente, de Z a A) */
 select idFunc as ID_Funcionario, nomeFunc as Nome_Funcionario from funcionarios order by nomeFunc desc;
 
-/*Buscar os acmpos ID com o apelido ID_Funcionario, nomeFunc com o apelido Nome_Funcionario e cargo com o apelido Cargo_Funcionario
-da tabela funcionários ordenado de forma descedente (do maior ID para o menor) */
 select idFunc as ID_Funcionario, nomeFunc as Nome_Funcionario, cargo as Cargo_Funcionario from funcionarios order by idFunc desc;
 
-/*Buscar os campos ID com o apelido ID_Funcionario, nomeFunc com o apelido Nome_Funcionario e cargo com o apelido Cargo_Funcionario
-da tabela funcionários ordenado de forma descedente (do maior ID para o menor) */
 select idFunc as ID_Funcionario, nomeFunc as Nome_Funcionario, cargo as Cargo_Funcionario from funcionarios  where cargo <> 'null' order by idFunc desc;
 
 select * from funcionarios  where cargo = 'Gerência' order by NomeFunc asc;
 
-
-/* Atualizar o campo login na tabela funcionarios especificando o id */
-update funcionarios set login = "felipecoelho" where idFunc = 6;
-
-update funcionarios set cargo = "Gerência" where idFunc = 2;
-
-update funcionarios set email = "lauralopes@gmail.com" where idFunc = 1;
-
-update funcionarios set email = "victoriacardoso@gmail.com" where idFunc = 2;
-
-update funcionarios set email = "brenosilva@gmail.com" where idFunc = 3;
-
-update funcionarios set email = "pamellapereto@gmail.com" where idFunc = 5;
-
-update funcionarios set email = "felipecoelho@gmail.com" where idFunc = 6;
-
-
-
-/*DELETE */
-
-delete from funcionarios where idFunc = 9;
-
 create table quartos (
 	idQuarto int primary key auto_increment,
     andar varchar(10) not null,
+    numeroQuarto varchar (10) not null,
     tipoQuarto varchar(50) not null,
     ocupacaoMax int not null,
 	situacao char(3) not null,
     nome varchar(50) not null,
     descricao text,
+    foto varchar (255) not null,
     preco decimal(10,2) not null,
+    cafeDaManha char (3) not null,
+    precoCafe decimal (10,2),
     tipoCama varchar(20),
     varanda char(3)
-    
 );
 
 describe quartos;
 
-delete from quartos where idQuarto = 2;
-
-alter table quartos change ocupacacaoMax ocupacaoMax int not null;
-
-alter table quartos add column numeroQuarto varchar(10) not null after andar;
-
-alter table quartos add column cafeDaManha char(3) not null after preco;
-alter table quartos add column foto varchar(255) not null after descricao; 
-
-insert into quartos (andar, numeroQuarto, tipoQuarto, ocupacaoMax, situacao, nome, descricao, preco, tipoCama, varanda) values ("5°", "505", "Superior Premier", "3", "não", 
+insert into quartos (andar, numeroQuarto, tipoQuarto, ocupacaoMax, situacao, nome, descricao, foto, preco, cafeDaManha, tipoCama, varanda) values ("5°", "505", "Superior Premier", "3", "não", 
 "Familiar", "O quarto de 32m² com piso frio, varanda - vista para o mar. Oferece ar condicionado individual, TV LCD 42'', wi-fi gratis, cofre digital, 
-frigobar abastecido e banheiro com secador de cabelo e amenities e mesa de trabalho.", 750.90, "Queen Size", "sim");
+frigobar abastecido e banheiro com secador de cabelo e amenities e mesa de trabalho.",  "https://www.revistasim.com.br/wp-content/uploads/2021/03/revistaSIM_Arquitetura_Quarto-de-hotel-em-casa_Destaque_Credito_Eduardo-Pozella-1155x500.jpg", 750.90,  "sim", "60.00", "Queen Size", "sim");
 
-insert into quartos (andar, numeroQuarto, tipoQuarto, ocupacaoMax, situacao, nome, descricao, preco, cafeDaManha, tipoCama, varanda) values ("10°", "14", "Suite Presidencial", "2", "não", 
+insert into quartos (andar, numeroQuarto, tipoQuarto, ocupacaoMax, situacao, nome, descricao, foto, preco, cafeDaManha, precoCafe, tipoCama, varanda) values ("10°", "14", "Suite Presidencial", "2", "não", 
 "Familiar", "O quarto de 40m² com piso frio, varanda - vista para o mar. Oferece ar condicionado individual, TV LCD 50'', wi-fi gratis, cofre digital, 
-frigobar abastecido e banheiro com secador de cabelo e amenities e mesa de trabalho.", 5000, "sim", "Queen Size", "sim");
+frigobar abastecido e banheiro com secador de cabelo e amenities e mesa de trabalho.",  "https://www.revistasim.com.br/wp-content/uploads/2021/03/revistaSIM_Arquitetura_Quarto-de-hotel-em-casa_Destaque_Credito_Eduardo-Pozella-1155x500.jpg", 5000, "sim", "60.00", "Queen Size", "sim");
 
 insert into quartos (andar, numeroQuarto, tipoQuarto, ocupacaoMax, situacao, nome, descricao, foto, preco, cafeDaManha, tipoCama, varanda) values ("5°", "33", "Room Standard", "2", "não", 
 "Casal", "O quarto de 30m² com piso frio, varanda. Oferece ar condicionado individual, TV LCD 40'', wi-fi gratis,
 frigobar abastecido e banheiro com secador de cabelo e amenities e mesa de trabalho.", "https://www.revistasim.com.br/wp-content/uploads/2021/03/revistaSIM_Arquitetura_Quarto-de-hotel-em-casa_Destaque_Credito_Eduardo-Pozella-1155x500.jpg",  409, "nao", "Queen Size", "sim");
-
-update quartos set cafeDaManha = "sim" where idQuarto = 1;
-update quartos set foto = "https://www.revistasim.com.br/wp-content/uploads/2021/03/revistaSIM_Arquitetura_Quarto-de-hotel-em-casa_Destaque_Credito_Eduardo-Pozella-1155x500.jpg" where idQuarto = 1;
-update quartos set foto = "https://www.hplus.com.br/wp-content/uploads/2019/08/quarto-cama-casal-hotel-vision-hplus-em-brasilia-13.jpg" where idQuarto = 3;
-
 
 select * from quartos;
 
@@ -162,9 +87,24 @@ select * from quartos order by preco desc;
 
 select * from quartos where preco < 700.00 and situacao = "nao";
 
+create table clientes (
+    idCliente int primary key auto_increment,
+    nomeCompleto varchar(100) not null,
+    cpf char(14) not null unique,
+    rg char(12) not null unique,
+    email varchar(50) unique,
+    celular varchar(20) not null
+);
 
-/*dataPedido timestap default current_timestap significa que a data do pedido será a mesma do sistema, ou seja, a data atual
-statusPedido significa que a situação do pedido será uma das seguintes opções: Pendente, Finalizado, Cancelado */
+describe clientes;
+
+insert into clientes (nomeCompleto, cpf, rg, email, celular) values
+("José de Assis", "829.942.570-09", "48.353.888-7", "josedeassis@gmail.com", "(96) 99338-2803");
+
+insert into clientes (nomeCompleto, cpf, rg, email, celular) values
+("Nathan Hitler", "452.230.750-78", "84.560.325-5", "nathanhitler@gmail.com", "(49) 99525-9820");
+
+select * from clientes;
 
 create table pedido (
     idPedido int primary key auto_increment,
@@ -174,22 +114,31 @@ create table pedido (
     foreign key (idCliente) references clientes (idCliente) 
 );
 
+describe clientes;
+
 insert into pedido (statusPedido, idCliente) values ("Pendente", 1);
+
 insert into pedido (statusPedido, idCliente) values ("Finalizado", 2);
 
 select * from pedido;
-select * from pedido inner join clientes on pedido.idCliente = clientes.idCliente;
 
-describe pedido;
+select * from pedido inner join clientes on pedido.idCliente = clientes.idCliente;
 
 create table reservas (
 	idReserva int primary key auto_increment, 
     idPedido int not null,
     idQuarto int not null,
     foreign key (idPedido) references pedido (idPedido),
-    foreign key (idQuarto) references quartos (idQuarto)
+    foreign key (idQuarto) references quartos (idQuarto),
+    checkin datetime not null,
+    checkout datetime not null
     
 );
+
+describe reservas;
+
+insert into reservas (idPedido, idQuarto, checkin, checkout) values (1, 1, "2023-11-02 14:00:00", "2023-11-05 12:00:00");
+insert into reservas (idPedido, idQuarto, checkin, checkout) values (1, 3, "2023-11-02 14:00:00", "2023-11-05 12:00:00");
 
 select * from reservas;
 
@@ -197,53 +146,3 @@ select reservas.idReserva, pedido.idPedido,
 quartos.idQuarto, quartos.nome, quartos.andar, quartos.numeroQuarto
 from (reservas inner join pedido on reservas.idPedido = pedido.idPedido)
 inner join quartos on reservas.idQuarto = quartos.idQuarto;
-
-
-
-insert into reservas (idPedido, idQuarto, checkin, checkout) values (1, 1, "2023-11-02 14:00:00", "2023-11-05 12:00:00");
-insert into reservas (idPedido, idQuarto, checkin, checkout) values (1, 3, "2023-11-02 14:00:00", "2023-11-05 12:00:00");
-
-
-alter table reservas add column checkin datetime not null;
-alter table reservas add column checkout datetime not null;
-
-describe reservas;
-    
-
-create table clientes (
-    idCliente int primary key auto_increment,
-    nomeCompleto varchar(100) not null,
-    cpf char(14) not null unique,
-    rg char(12) not null unique,
-    email varchar(50) unique,
-    celular varchar(20) not null
-   
-);
-
-describe clientes;
-
-drop table clientes;
-
-insert into clientes (nomeCompleto, cpf, rg, email, celular) values
-("José de Assis", "829.942.570-09", "48.353.888-7", "josedeassis@gmail.com", "(96) 99338-2803");
-
-
-insert into clientes (nomeCompleto, cpf, rg, email, celular) values
-("Nathan Hitler", "452.230.750-78", "84.560.325-5", "nathanhitler@gmail.com", "(49) 99525-9820");
-select * from clientes;
-
-/* Busacr o nome completo e o celular do cliente que alugou o quarto de número 14, pois a tabela quartos está vinculada á tabela clientes pelo campo idQuarto */ 
-select clientes.nomeCompleto,
-clientes.celular
-from quartos inner join clientes
-on quartos.idQuarto = clientes.idQuarto where numeroQuarto = 14;
-
-/* Buscar TODAS AS INFORMAÇÕES da tabela quartos que está vinculada á tabela clientes pelo campo idQuarto */
-select * from quartos inner join clientes
-on quartos.idQuarto = clientes.idQuarto;
-
-/* Buscar o nome completo e data/horário do checkout do cliente que alugou o quarto  de número 14 */
-select clientes.nomeCompleto, date_format (clientes.checkout,'%d/%m/%Y - %H:%i') as Checkout  from quartos inner join clientes on quartos.idQuarto = clientes.idQuarto where numeroQuarto = 14; 
-
-
-
